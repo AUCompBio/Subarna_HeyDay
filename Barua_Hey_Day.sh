a1aa829b40c264ce3f28bd6f1bfedfbdf68d0796
Subarna Barua <71520410+subarnabarua@users.noreply.github.com>
2020-10-10 16:41:02 -0500

    Add files via upload

---
+++ Barua_Hey_Day.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#Students is prompted to type name
+echo 'Welcome to HEYDAY! Please enter your name:'
+
+#read assigns user name input to variable name
+read name
+
+#print statement to the student
+echo "Hey, $name!"
+
